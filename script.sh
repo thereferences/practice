651cd24860151d09444a032efe2d4cb71d995b8e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-18 21:28:09 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 : << 'comment'
-The set up herein is must be in line ...
+    The set up herein is must be in line ...
 comment
 
-# The environment in focus
+: << 'environment'
+    The environment in focus
+environment
 prefix=/mnt/j/programs/anaconda3/envs/practice
 
 : << 'delete'
-  Delete the existing <practice> environment
+    Delete the existing <practice> environment
 delete
 conda remove -y --prefix $prefix --all
 
 : << 'rebuild'
-  Rebuild environment <development> via a requirements.txt file
+    Rebuild environment <development> via a requirements.txt file
 rebuild
 conda env create -f environment.yml -p $prefix
