d0a4fa76edc282791e288b610c766a265012571e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-20 16:31:17 +0000

    Update environment.sh

--- environment.sh
+++ environment.sh
@@ -6,16 +6,17 @@ requirements.txt file to create an environment.
 comment
 
 # The environment in focus
-prefix=/mnt/j/programs/anaconda3/envs/development
+prefix=/mnt/j/programs/anaconda3/envs/practice
 
 : << 'delete'
-  Delete the existing <development> environment
+  Delete the existing <practice> environment
 delete
 conda remove -y --prefix $prefix --all
 
 : << 'rebuild'
-  Rebuild environment <development> via a requirements.txt file
+  Rebuild environment <practice> via environment.yml
 rebuild
-conda create -y --prefix $prefix -c conda-forge  python==3.11.7
-conda activate development
-conda install -y --prefix $prefix -c conda-forge --file requirements.txt
+conda env create -f environment.yml -p $prefix
+# conda create -y --prefix $prefix -c conda-forge  python==3.11.7
+# conda activate development
+# conda install -y --prefix $prefix -c conda-forge --file requirements.txt
