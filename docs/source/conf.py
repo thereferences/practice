0ac4c50c50130db339630f795b6a5fc599fe66b6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-01-09 16:49:47 +0000

    Update docs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,7 @@ https://www.sphinx-doc.org/en/master/usage/configuration.html#project-informatio
 
 import sphinx_rtd_theme
 import sphinx_design
+import furo
 
 '''
 Basic
@@ -52,7 +53,7 @@ Options for HTML output
 https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 '''
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 
 html_static_path = ['_static']
 
